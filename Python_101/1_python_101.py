1c111fc8e9e8bdaee0c09865fefcdb95c01928cc
yangyang193 <642265568@qq.com>
2026-01-06 05:16:05 +0800

    Delete Python_101 directory

--- Python_101/1_python_101.py
+++
@@ -1,5 +0,0 @@
-name = "Tongzhou Yu"
-print(f"Hello, {name}!")
-x = "1"
-y = "2"
-print(x+y)
--- Python_101/4.2_memory_refactored/roles.py
+++
@@ -1,8 +0,0 @@
-def get_role_prompt(role_name):
-    """根据角色名获取角色设定"""
-    pass
-
-def get_break_rules():
-    """获取结束对话的规则说明"""
-    pass
-
